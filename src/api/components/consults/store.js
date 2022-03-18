0d35bd4b33d53102444d34f6c5719f44266e1dc0
xVyrusz <cisnnerosbcrra@hotmail.com>
2022-03-17 18:02:41 -0600

    Corregido createCosultData

--- src/api/components/consults/store.js
+++ src/api/components/consults/store.js
@@ -11,10 +11,10 @@ const createConsult = async (data) => {
 
 const createConsultData = async (data) => {
     return await Consulta.create({
-        testMade: data.testMade,
-        diagnosis: data.diagnosis,
-        tratamiento: data.tratamiento,
-        idTest: data.idTest
+        dateVisit: data.dateVisit,
+        idDoctor: data.idDoctor,
+        idPatient: data.idPatient,
+        idReason: data.idReason
     });
 };
 
